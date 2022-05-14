53894b710bbe47f0b76a41379454ca84f0ea859f
yumin-jung <jungym887@naver.com>
2022-05-14 13:00:15 +0900

    Update constants

--- src/atoms/constants.js
+++ src/atoms/constants.js
@@ -13,8 +13,14 @@ export const RENDER_WIDTH = 1200;
 export const RENDER_HEIGHT = 600;
 export const BIRD_X = 250;
 export const BIRD_Y = 450;
-export const BIRD_SIZE_RED = 20;
+export const BIRD_SIZE_RED = 25;
+export const BIRD_SIZE_CHUCK = 40;
+export const BIRD_SIZE_BOMB = 40;
+export const BIRD_SIZE_HAL = 40;
+export const BIRD_SIZE_MATILDA = 40;
 export const PIG_SIZE_MINION = 20;
+export const PIG_SIZE_CORPORAL = 25;
+export const PIG_SIZE_KING = 50;
 
 export const GROUND_HEIGHT = 30;
 export const GROUND_X = RENDER_WIDTH / 2;
